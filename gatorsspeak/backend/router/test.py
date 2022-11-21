f61ab912da6df2a5d4b175b635c3d5730bc37390
LindeJuan <110645289+LindeJuan@users.noreply.github.com>
2022-11-20 19:53:17 -0500

    Added more test cases for reading from test database

--- gatorsspeak/backend/router/test.py
+++ gatorsspeak/backend/router/test.py
@@ -17,6 +17,15 @@ router = APIRouter(
 def instert_data_to_test_table(request:TestBase, db: Session = Depends(get_db)):
     return db_test.insert_to_test_table(db, request)
 
+# Read all data in test table
+@router.get('/')
+def get_all_data_in_test_table(db: Session = Depends(get_db)):
+    return db_test.read_all_data_from_test_table(db)
+
+# Read data by id
+@router.get('/{id}')
+def get_test_table_data_by_id(id:int, db: Session = Depends(get_db)):
+    return db_test.read_data_from_test_table_by_id(db, id)
 
 # Read data from test table
 
